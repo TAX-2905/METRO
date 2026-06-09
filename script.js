4e31c6619048211fe28b4d8c86be825fb2d1c35c
lomesh takoory <lomesh.takoory1@umail.uom.ac.mu>
2026-06-09 20:55:09 +0400

    fontsizw

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function getFormattedDate() {
 
 function drawHorizontallyCenteredText(ctx, text, centerX, y) {
     // Set the font before measuring
-    ctx.font = "600 55px 'IBM Plex Sans'";
+    ctx.font = "600 50px 'IBM Plex Sans'";
   
     // Measure the width of the text
     const textWidth = ctx.measureText(text).width;
@@ -64,7 +64,7 @@ function drawHorizontallyCenteredText(ctx, text, centerX, y) {
           ctx.drawImage(afternoonImg, 0, 0);
       }
   
-      ctx.font = "600 55px 'IBM Plex Sans', sans-serif";
+      ctx.font = "600 50px 'IBM Plex Sans', sans-serif";
       ctx.fillStyle = "black";
       drawHorizontallyCenteredText(ctx, day, 2565, 5555);
       ctx.fillText(date, 2359, 5650);
